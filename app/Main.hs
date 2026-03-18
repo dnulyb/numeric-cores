36abfec521ae12a9043092603465d13aa981a0a0
dnulyb <137195773+dnulyb@users.noreply.github.com>
2026-03-18 15:56:51 +0100

    Ensure minimum is never called on empty list

--- app/Main.hs
+++ app/Main.hs
@@ -39,9 +39,9 @@ funcs = [safeSub, safeMult, safeDiv]
 core :: [Double] -> Maybe Int
 core numbers
     | null res = Nothing
+    | otherwise = minimum $ map (round <$>) res
     -- Core is valid only if the result is an integer after the complete calculation
-    | otherwise = minimum $ map (round <$>) $ filter isInteger res
-    where res = filter isJust [eval numbers p | p <- permutations funcs]
+    where res = filter isInteger [eval numbers p | p <- permutations funcs]
           eval :: [Double] -> [Double -> Double -> Maybe Double] -> Maybe Double
           eval (n:ns) fs = foldM (\acc (f, x) -> acc `f` x) n (zip fs ns)
           eval _ _ = Nothing
