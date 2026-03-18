1811bd85bc2fe0bbcd3878307c3fe14e17cb19c0
dnulyb <137195773+dnulyb@users.noreply.github.com>
2026-03-18 12:47:15 +0100

    Core calculations can now be fractions in the middle of the calculation, but they must be integers after the final step in order to be valid.

--- app/Main.hs
+++ app/Main.hs
@@ -8,47 +8,46 @@ import Data.Char(toLower, ord, chr)
 main :: IO ()
 main = do
     print $ core $ toList4 5 2 4 6        -- should be: Just 1
-    print $ core $ toList4 1000 400 20 5  -- should be: Just 150
+    print $ core $ toList4 1000 400 20 5  -- should be: Just 45
     print $ core $ toList4 1 2 3 4        -- should be: Nothing
     print $ coreStr "ebdf"                -- should be: Just 'a'
     print $ coreStr "utah"                -- should be: Just 'h'
+    print $ coreStr "hand"                -- should be: Just 'b'
 
-toList4 :: Int -> Int -> Int -> Int -> [Int]
+toList4 :: Double -> Double -> Double -> Double -> [Double]
 toList4 a b c d = [a,b,c,d]
 
-safeSub :: Int -> Int -> Maybe Int
+safeSub :: Double -> Double -> Maybe Double
 safeSub x y
     | res >= 0 = Just res
     | otherwise = Nothing
     where res = x-y
 
-safeMult :: Int -> Int -> Maybe Int
+safeMult :: Double -> Double -> Maybe Double
 safeMult x y
     | res >= 0 = Just res
     | otherwise = Nothing
     where res = x*y
 
-safeDiv :: Int -> Int -> Maybe Int
+safeDiv :: Double -> Double -> Maybe Double
 safeDiv _ 0 = Nothing
-safeDiv x y
-    | isInteger res = Just $ round res
-    | otherwise = Nothing
-    where res = fromIntegral x / fromIntegral y
-          isInteger :: Double -> Bool
-          isInteger n = n == fromInteger (round n)
+safeDiv x y = Just $ x / y
 
-
-funcs :: [Int -> Int -> Maybe Int]
+funcs :: [Double -> Double -> Maybe Double]
 funcs = [safeSub, safeMult, safeDiv]
 
-core :: [Int] -> Maybe Int
+core :: [Double] -> Maybe Int
 core numbers
     | null res = Nothing
-    | otherwise = minimum res
+    -- Core is valid only if the result is an integer after the complete calculation
+    | otherwise = minimum $ map (round <$>) $ filter isInteger res
     where res = filter isJust [eval numbers p | p <- permutations funcs]
-          eval :: [Int] -> [Int -> Int -> Maybe Int] -> Maybe Int
+          eval :: [Double] -> [Double -> Double -> Maybe Double] -> Maybe Double
           eval (n:ns) fs = foldM (\acc (f, x) -> acc `f` x) n (zip fs ns)
           eval _ _ = Nothing
+          isInteger :: Maybe Double -> Bool
+          isInteger (Just n) = n == fromInteger (round n)
+          isInteger Nothing = False
 
 -- String length must be exactly 4
 coreStr :: String -> Maybe Char
@@ -56,8 +55,8 @@ coreStr [a,b,c,d]
     | isNothing res = Nothing
     | (fromJust res < 1) || (fromJust res > 26) = Nothing   -- Out of bounds for alphabet
     | otherwise = Just $ chr $ (+) 96 $ fromJust res
-    where convert :: [Char] -> [Int]
-          convert = map (subtract 96 . ord . toLower)
+    where convert :: [Char] -> [Double]
+          convert = map (subtract 96 . fromIntegral . ord . toLower)
           res = core $ convert [a,b,c,d]
 coreStr _ = Nothing
 
